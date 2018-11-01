0fff32a15e608648e13937919cb06477f4679842
softberry <emre.sakarya@softberry.de>
2018-11-02 00:06:26 +0100

    Project complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import BoatTour from "./main";
+/**
+ * I prefer to use Plain JavaScript, so to keep project entry point as is,
+ * I have initialized the project from index.ts file,
+ */
+/**
+ * Import all initially required modules
+ */
+import BoatTour from "./boattour";
+import State from "./lib/state";
 import { default as Stage } from "./lib/stage.js";
-
+/**
+ * Wait for DOM to be ready
+ */
 window.addEventListener("DOMContentLoaded", () => {
-  new BoatTour(Stage);
+  new BoatTour(Stage, new State());
 });
--- src/lib/guide.js
+++ src/lib/guide.js
@@ -1,5 +1,10 @@
+/**
+ * @description prepare axis and moving point
+ * @class Guide
+ */
 class Guide {
   constructor(container) {
+    this.data = {};
     this.axis = document.createElement("div");
     this.point = document.createElement("div");
     this.axis = document.createElement("div");
@@ -9,44 +14,75 @@ class Guide {
 
     this.counter = 0;
   }
-
+  /**
+   * @description Prepare invisible guide element
+   */
   styleElements() {
     this.axis.style.width = "80px";
     this.axis.style.height = "1px";
     this.axis.style.position = "absolute";
     this.axis.style.left = "0";
     this.axis.style.top = "0";
-    this.axis.style.backgroundColor = "black";
     this.axis.style.transformOrigin = "left center";
-    // this.axis.style.transition = "all 1s";
 
     this.point.style.width = "1px";
     this.point.style.height = "1px";
     this.point.style.position = "absolute";
     this.point.style.left = "0";
-    this.point.style.backgroundColor = "yellow";
   }
-
+  /**
+   * @description setter for Rotation angle
+   * @param {number} num
+   */
   setRotation(num) {
-    this.axis.dataset.rotateFrom = num.toString();
+    this.data.from = num;
     this.axis.style.transform = `rotate(${num}deg)`;
   }
-
+  /**
+   * Setter for Maximum rotation angle
+   * @param {number} num
+   */
   setMaxRotation(num) {
-    this.axis.dataset.rotateTo = num.toString();
+    this.data.to = num;
   }
-
+  /**
+   * Getter for rotation angle
+   */
   getRotation() {
-    return parseFloat(this.axis.dataset.rotateFrom);
-  }
-  getMaxRotation() {
-    return parseFloat(this.axis.dataset.rotateTo);
+    return this.data.from;
   }
 
+  /**
+   * Getter for max rotation angle
+   */
+  getMaxRotation() {
+    return this.data.to;
+  }
+  /**
+   * @description Increses current rotation angle by 1 degree
+   * current rotation angle may be positive but the target can be negative,
+   * so it should be considered accordingly to calculate increase
+   */
   increaseRotation() {
     let r = this.getRotation();
-    if (r > this.getMaxRotation()) return;
-    r++;
+    const max = this.getMaxRotation();
+
+    if (max >= 0) {
+      if (r < max) {
+        r++;
+      }
+      if (r > max) {
+        r--;
+      }
+    } else {
+      // r--;
+      if (r > max) {
+        r--;
+      }
+      if (r < max) {
+        r++;
+      }
+    }
     this.setRotation(r);
     return r;
   }
